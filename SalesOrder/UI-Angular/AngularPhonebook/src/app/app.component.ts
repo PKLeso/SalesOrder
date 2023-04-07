d003c43bb6cea83207c807c6a999576496d476b6
Kagiso <percykgs2@gmail.com>
2023-04-07 12:28:58 +0200

    Updated appComponent

--- SalesOrder/UI-Angular/AngularPhonebook/src/app/app.component.ts
+++ SalesOrder/UI-Angular/AngularPhonebook/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { SignalrService } from './Shared/signalr.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent  implements OnInit, OnDestroy{
   title = 'Phonebook App';
